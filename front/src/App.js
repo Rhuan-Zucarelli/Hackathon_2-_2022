e49f4ddbc7570cd9edd3fd61a83532a0102f0da2
Rhuan-Zucarelli <rhuan1zucrelli@gmail.com>
2022-11-14 21:55:49 -0300

    Criando a ligação com a API

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,30 @@
+import React, { useEffect, useState } from "react";
 import Navbar from "./componentes/Navbar";
 
 function App() {
+  const [character, setCharacters] = useState();
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+    fetch("url")
+      .then((Response) => Response.json())
+      .then((data) => setCharacters(data.results))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, []);
+
   return (
-    
-    /* Navegação */
-    <Navbar brand = {"Almanaque Rick and Morty"} />
-    
+    <>
+      {/* Barra de navegação */}
+      <Navbar brand={"Almanaque Rick and Morty"} 
+
+      />
+
+    </>
   );
 }
 
