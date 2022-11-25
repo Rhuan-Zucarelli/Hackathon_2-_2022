b6884a45b2007bc86ea29f7cfbd6ef2348a20b01
Rhuan-Zucarelli <rhuan1zucrelli@gmail.com>
2022-11-25 19:02:33 -0300

    Finalização

--- front/src/pagina/Sobre/index.jsx
+++ front/src/pagina/Sobre/index.jsx
@@ -1,12 +1,12 @@
 import Footer from "../../componentes/Footer";
 import Navbar from "../../componentes/Nav";
 import logo from "../../Images/logo.png";
-
+import "./../Sobre/index.css";
 
 function Sobre() {
   return (
     <>
-      <div className=" ">
+      <div>
         <div>
           <Navbar brand={"Almanaque Rick and Morty"} sobre={"Sobre"} />
           <div
@@ -15,32 +15,29 @@ function Sobre() {
               padding: "20px",
             }}
           >
-            <h1>Almaneque Rick And Morty</h1>
+            <h1>Almanaque Rick And Morty</h1>
 
-            <img
-              src={logo}
-              style={{
-                width: "600px",
-                height: "600px",
-              }}
-            />
+            <img className="imgSobre" src={logo} />
 
-            <h3>Para que foi criado :</h3>
-            <p>Este site tem o intuito de lhe apresentar todos os personagens que já
-          apareceram durante a serie de Rick and Morty, a serie tem como principal 
-          integrante o Morty que é neto do avô mais inteligente de todas as galaxias 
-          Rick a mente mais brilhante em todo o universo e também o mais niilista de todos. 
-          Eles vivem junto grandes aventuras nas mais diversas realidades possíveis,  
-          totalmente escatológico, nonsense e politicamente incorreto, do jeitinho que
-          a adult swim gosta! wabba lubba dub dub!!!
-          </p>
-
-            
+            <h3>Para que foi criado</h3>
+            <p style={
+              {
+                padding:"50px  100px 50px 100px"
+              }
+            }>
+              Este site tem o intuito de lhe apresentar todos os personagens que
+              já apareceram durante a serie de Rick and Morty, a serie tem como
+              principal integrante o Morty que é neto do avô mais inteligente de
+              todas as galaxias Rick a mente mais brilhante em todo o universo e
+              também o mais niilista de todos. Eles vivem junto grandes
+              aventuras nas mais diversas realidades possíveis, totalmente
+              escatológico, nonsense e politicamente incorreto, do jeitinho que
+              a adult swim gosta! wabba lubba dub dub!!!
+            </p>
           </div>
-          </div>
-       
+        </div>
       </div>
-      <Footer name="Desenvolvido por Rhuan e Murilo"/> 
+      <Footer name="Desenvolvido por Rhuan e Murilo" />
     </>
   );
 }
